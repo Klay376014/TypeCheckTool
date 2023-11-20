b7f78d9f5082329a34ae283a9aaa53345e326944
klay <ads1029384756@gmail.com>
2023-11-20 22:32:28 +0800

    fix: change variable zhHan to zhTw

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,5 +1,5 @@
 import { createI18n } from 'vue-i18n'
-import zhHant from '../locales/zh-tw.json'
+import zhTw from '../locales/zh-tw.json'
 import en from '../locales/en.json'
 import ja from '../locales/ja.json'
 
@@ -7,9 +7,9 @@ export default defineNuxtPlugin(({ vueApp }) => {
   const i18n = createI18n({
     legacy: false,
     globalInjection: true,
-    locale: 'zhHant',
+    locale: 'zhTw',
     messages: {
-      zhHant,
+      zhTw,
       en,
       ja
     }
